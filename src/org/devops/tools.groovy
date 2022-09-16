51b6b9e68abe7cf4b650f872278fd622a36640e8
kc-fantasy <37774789+kc-fantasy@users.noreply.github.com>
2022-09-16 14:45:36 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,14 +1,14 @@
 package org.devops
 
 //带颜色打印信息
-def PrintMsg(content, color) {
-    colors = ['red'   : "\033[40;31m >>>>>>>>>>>${content}<<<<<<<<<<< \033[0m",
-              'blue'  : "\033[47;34m ${content} \033[0m",
-              'green' : "\033[40;32m >>>>>>>>>>>${content}<<<<<<<<<<< \033[0m" ]
-    ansiColor('xterm') {
-        println(colors[color])
-    }
-}
+// def PrintMsg(content, color) {
+//     colors = ['red'   : "\033[40;31m >>>>>>>>>>>${content}<<<<<<<<<<< \033[0m",
+//               'blue'  : "\033[47;34m ${content} \033[0m",
+//               'green' : "\033[40;32m >>>>>>>>>>>${content}<<<<<<<<<<< \033[0m" ]
+//     ansiColor('xterm') {
+//         println(colors[color])
+//     }
+// }
 
 // 获取时间
 def getTime() {
